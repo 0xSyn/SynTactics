3ad2840e6290c042f5a085dbdef265c45dfc54cb
0xSyn <31738750+0xSyn@users.noreply.github.com>
2018-04-10 14:19:52 -0400

    ...

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -9,43 +9,33 @@ namespace Assets.Scripts {
         public int _mapHeight;
 
         private BoardPathing _path;
+        private UI_manager _UI;
         private GameObject _blockSelected;
         private GameObject _blockSelected2;
         private GameObject _blockHovered;
         private GameObject[] blockSubset=new GameObject[100];
         private int blockSubset_pointer;
-        public Canvas UI;
-        public GameObject UI_ally;
-        public GameObject UI_selected;
-        private bool _movementMode = false;
+        private bool _movementMode;
 
         void Start() {
             _path = GetComponent<BoardPathing>();
-            UI_ally.SetActive(false);
-            UI_selected.SetActive(false);
-
+            _UI = GetComponent<UI_manager>();
         }
 
         void Update() {
-            if(Input.GetMouseButtonDown(1)){
-                //Debug.Log("RMB");
+            if(Input.GetMouseButtonDown(1)){///RMB
                 if (_path.PathSize() > 1) {
                     _path.RemovePartialPath(1);
                 }
                 else {
                     _movementMode = false;
                     _blockSelected = null;
-                    UI_ally.SetActive(false);
+                    _UI.SetActionMenuVisible(false);
                     _path.ClearBlockSubset();
                 }
             }
-            if (Input.GetMouseButtonDown(2)) {
-                //Debug.Log("MMB");
+            if (Input.GetMouseButtonDown(2)) {//MMB
             }
-            if (Input.GetKey(KeyCode.U)) {
-                _path.Move();
-            }
-
         }
 
 
@@ -62,7 +52,8 @@ namespace Assets.Scripts {
             _blockSelected2 = null;
             _movementMode = false;
             _path.Move();
-            UI_ally.SetActive(false);
+
+            _UI.SetActionMenuVisible(false);
         }
 
 
@@ -70,7 +61,6 @@ namespace Assets.Scripts {
         public void AddToPath(int id) {
             if (_movementMode) {
                 if (_blockSelected2=_path.IsNodeInSubset(id)) {
-                    //Debug.Log("ID: " + _blockSelected2.GetComponent<NodeData>().blockID);
                     _path.BuildPath(_blockSelected2);
                 }
                 else {
@@ -79,16 +69,13 @@ namespace Assets.Scripts {
             }
         }
 
-        //public void SetHovered(int col, int row) {
-        //    _blockHovered = _map[col, row];
-        //}
 
 
 
 
         public void Selected(int x, int y) {
             _blockSelected=_map[x, y];
-            UI_ally.SetActive(true);
+            _UI.SetActionMenuVisible(true);
             
         }
 
@@ -96,11 +83,14 @@ namespace Assets.Scripts {
             return _movementMode;
         }
 
-        public void BTN_Move() {
-            if (_blockSelected != null) {   
-                _movementMode = true;
-                _path.BuildPath(_blockSelected);
+        public void SetMovementMode(bool mode_on) {
+            if (mode_on) {
+                if (_blockSelected != null) {
+                    _movementMode = true;
+                    _path.BuildPath(_blockSelected);
+                }
             }
+            else { }
         }
 //_________________________________________________________________________________
     }
---
+++ Assets/Scripts/UI_manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Assets.Scripts {
+    public class UI_manager : MonoBehaviour {
+        BoardManager _boardManager;
+        public GameObject UI_ActionMenu;
+        public GameObject UI_UnitData;
+        // Use this for initialization
+        void Start() {
+            _boardManager = GetComponent<BoardManager>();
+            UI_ActionMenu.SetActive(false);
+            UI_UnitData.SetActive(false);
+        }
+
+        public void SetActionMenuVisible(bool visible) {
+            UI_ActionMenu.SetActive(visible);
+        }
+        public void SetUnitDataVisible(bool visible) {
+            UI_UnitData.SetActive(visible);
+        }
+
+
+        public void BTN_Move() {
+            _boardManager.SetMovementMode(true);
+        }
+
+        public void BTN_attack() {
+
+        }
+    }
+}
