4bdab96ccc1c1f97a9d9db11972109b68f4b2375
jay <beej@planetmailnet>
2018-04-12 17:22:20 -0400

    Moar

---
+++ Assets/Scripts/LevelEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEditor : MonoBehaviour {
+    public GameObject Block;
+    private GameObject[,] _board;
+    public void LoadBlocks(int x,int y) {
+        _board=new GameObject[x,y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                _board[i,j] = Instantiate(Block, new Vector3(i, 0, j), Quaternion.identity, transform);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/LevelEditor_UI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelEditor_UI : MonoBehaviour {
+    private LevelEditor _editor;
+    public InputField in_x;
+    public InputField in_y;
+    public InputField in_title;
+
+    void Start() {
+        _editor = GetComponent<LevelEditor>();
+    }
+
+    public void BTN_LoadBlocks() {
+        _editor.LoadBlocks(Int32.Parse(in_x.text), Int32.Parse(in_y.text));
+    }
+}
